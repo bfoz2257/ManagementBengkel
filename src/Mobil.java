49f2047ea256463b3e77ea578b6008c7fade9f6d
RizqiAdittiya <147025464+bfoz2257@users.noreply.github.com>
2024-11-01 15:35:50 +0700

    Update Mobil.java

--- src/Mobil.java
+++ src/Mobil.java
@@ -1,15 +1,14 @@
 class Mobil extends Kendaraan {
     private int jumlahPintu;
 
-    // Constructor dengan parameter
     public Mobil(String noPlat, String merk, String model, int tahun, int jumlahPintu) {
         super(noPlat, merk, model, tahun);
         this.jumlahPintu = jumlahPintu;
     }
 
-    // Constructor kosong
+    
     public Mobil() {
-        super();  // Memanggil constructor kosong dari Kendaraan
+        super(); 
         this.jumlahPintu = 0;
     }
 
